227422a44c7e403b4ba1aab3ae9d38ffe657bb84
ServerMeta <alberto.websci@gmail.com>
2015-02-20 14:21:45 +0100

    theater fixes

--- js/theater.voices.js
+++ js/theater.voices.js
@@ -1,8 +1,25 @@
 var theater = new TheaterJS();
+
                    
-theater.describe("Vader", .8, "#theater");
 theater
- .write("Vader:... Imagine a web without bottlenecks... ", 500)
- .write("Where content instead of being distributed from servers to clients ", 1000)
- .write("is shared directly between users. Without mediation.", 1000)
- .write(" Today this is possible, the web just got bigger.", 500);
+ .describe("Line1", .8, "#theater1")
+ .describe("Line2", .8, "#theater2");
+
+
+theater
+ .write("Line2:")
+ .write("Line1:Imagine a web without bottlenecks... ")
+ .write(500)
+ .write("Line1:Where content instead of being distributed from servers to clients ")
+ .write(250)
+ .write("Line2:is shared directly between users. Without mediations.")
+ .write(1000)
+ .write("Line2:")
+ .write("Line1:Today this is possible,")
+ .write(250)
+ .write("Line2:The web just got bigger.")
+ .write(5000)
+ .write(function () { theater.play(true); });
+
+
+
