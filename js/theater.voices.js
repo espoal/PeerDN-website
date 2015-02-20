e6ff4c38618c8fa93f307e8e132826c17e2c1051
ServerMeta <alberto.websci@gmail.com>
2015-02-20 12:44:02 +0100

    theater creation

---
+++ js/theater.voices.js
@@ -0,0 +1,8 @@
+var theater = new TheaterJS();
+                   
+theater.describe("Vader", .8, "#theater");
+theater
+ .write("Vader:... Imagine a web without bottlenecks... ", 500)
+ .write("Where content instead of being distributed from servers to clients ", 1000)
+ .write("is shared directly between users. Without mediation.", 1000)
+ .write(" Today this is possible, the web just got bigger.", 500);
